17e91a543b7265363fcc7c136022f475ed9b5b91
fewspider <fewspider@gmail.com>
2015-08-29 18:41:11 +0800

    update gitignore

--- Example/Pods/Target Support Files/SwiftChromeActivity/SwiftChromeActivity-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftChromeActivity : NSObject
-@end
-@implementation PodsDummy_SwiftChromeActivity
-@end
--- Example/Pods/Target Support Files/SwiftChromeActivity/SwiftChromeActivity-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double SwiftChromeActivityVersionNumber;
-FOUNDATION_EXPORT const unsigned char SwiftChromeActivityVersionString[];
-
